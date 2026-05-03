18eb3d86aa248cf9282014d5ea528b1d1a2527ab
atara57769 <atara57769@gmail.com>
2026-05-03 17:33:27 +0300

    add cli file

---
+++ cli.py
@@ -0,0 +1,26 @@
+import logging
+import argparse
+import sys
+from services.attendance_report_service import AttendanceReportService
+
+logger = logging.getLogger(__name__)
+
+def main():
+    parser = argparse.ArgumentParser(description="Attendance Report CLI Tool")
+    parser.add_argument("input", help="Path to the input PDF file")
+    parser.add_argument("-o", "--output", help="Path to the output directory", default=".")
+    
+    args = parser.parse_args()
+
+    service = AttendanceReportService(output_dir=args.output)
+    
+    try:
+        logger.info(f"Starting to process: {args.input}")
+        service.process(args.input) 
+        logger.info(f"Processed {args.input} successfully. Output saved to {args.output}")
+    except Exception as e:
+        logger.error(f"Error processing report: {e}")
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -1,31 +1,11 @@
 import logging
-import argparse
-import sys
-from services.attendance_report_service import AttendanceReportService
+from cli import main
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s | %(levelname)-8s | %(name)s:%(funcName)s:%(lineno)d - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
-logger = logging.getLogger(__name__)
-
-def main():
-    parser = argparse.ArgumentParser(description="Attendance Report CLI Tool")
-    parser.add_argument("input", help="Path to the input PDF file")
-    parser.add_argument("-o", "--output", help="Path to the output directory", default=".")
-    
-    args = parser.parse_args()
-
-    service = AttendanceReportService(output_dir=args.output)
-    
-    try:
-        logger.info(f"Starting to process: {args.input}")
-        service.process(args.input) 
-        logger.info(f"Processed {args.input} successfully. Output saved to {args.output}")
-    except Exception as e:
-        logger.error(f"Error processing report: {e}")
-        sys.exit(1)
 
 if __name__ == '__main__':
     main()
